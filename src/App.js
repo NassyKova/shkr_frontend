7b008133a9fa2d1d2a296a197283b69b527103de
Nassy Kova <mailtosummer@gmail.com>
2023-02-22 10:41:28 +1100

    Add IsLoading

--- src/App.js
+++ src/App.js
@@ -4,20 +4,42 @@ import "./App.css";
 import { GlobalStyle } from "./GlobalStyle";
 import { ProductList } from "./components/ProductList";
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import SpinnerBlue from "./components/Spinner";
 import NavBar from "./components/mui/NavBar";
+import { useState } from "react";
+import Box from "@mui/material/Box"
+import CircularProgress from "@mui/material/CircularProgress"
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 2000);
     return (
-        <div className="App">
-            <GlobalStyle />
-            <Header/>
-            <NavBar/>
-            <ProductList />
-            <SpinnerBlue />
-            <Footer />
-        </div>
+        <>
+            {isLoading ? (
+                <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: '100vh'
+                }}
+            >
+                <SpinnerBlue />
+                </Box>
+            ) : (
+                <div className="App">
+                    <GlobalStyle />
+                    <Header />
+                    <NavBar />
+                    <ProductList />
+                    <Footer />
+                </div>
+            )}
+        </>
     );
 }
 
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,9 +2,9 @@ import Spinner from "react-bootstrap/Spinner";
 
 function SpinnerBlue() {
     return (
-        <>
-            <Spinner animation="border" variant="info" />
-        </>
+        <Spinner animation="border" role="status" variant="info">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>
     );
 }
 
