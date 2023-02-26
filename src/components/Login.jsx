1f74c7de003ae600634dcedd55b450bb2c817503
Nassy Kova <mailtosummer@gmail.com>
2023-02-27 10:47:08 +1100

    Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,142 @@
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
 
-const Login = () => {
-    return
+import Title from "./styled/Title";
+
+const InputWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 400px;
+    margin-bottom: 10px;
+`;
+
+function Login() {
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [errorMessage, setErrorMessage] = useState({
+        username: null,
+        password: null,
+        apiError: null,
+    });
+
+    const [userFetched, setUserFetched] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(user);
+        let haveError = false;
+        if (!user.username) {
+            setErrorMessage((prevErrorMessage) => {
+                return {
+                    ...prevErrorMessage,
+                    username: "Username must be provided",
+                };
+            });
+            haveError = true;
+        }
+
+        if (!user.password) {
+            setErrorMessage((prevErrorMessage) => {
+                return {
+                    ...prevErrorMessage,
+                    password: "Password must be provided",
+                };
+            });
+            haveError = true;
+        }
+
+        if (!haveError) {
+            setErrorMessage({
+                username: null,
+                password: null,
+                apiError: null,
+            });
+            axios
+                .post("/auth/login", user)
+                .then((res) => res.data)
+                .then((json) => {
+                    setUserFetched(true);
+                    localStorage.setItem("token", json.token);
+                    console.log(json);
+                })
+                .catch(() => {
+                    setErrorMessage((prevErrorMessage) => {
+                        return {
+                            ...prevErrorMessage,
+                            apiError: "Username/Password doesn't exist",
+                        };
+                    });
+                });
+        }
+    };
+
+    const handleOnChange = (event) => {
+        setUser((prevUser) => {
+            return {
+                ...prevUser,
+                [event.target.name]: event.target.value,
+            };
+        });
+    };
+
+    return (
+        <>
+            {userFetched ? (
+                <Title>Login Successful</Title>
+            ) : (
+                <div>
+                    <Title>Login</Title>
+                    <form
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                        onSubmit={handleSubmit}
+                    >
+                        <InputWrapper>
+                            <label htmlFor="username">Username:</label>
+                            <input
+                                type="text"
+                                name="username"
+                                value={user.username}
+                                onChange={handleOnChange}
+                            />
+                        </InputWrapper>
+                        {errorMessage.username}
+                        <InputWrapper>
+                            <label htmlFor="password">Password:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                value={user.password}
+                                onChange={handleOnChange}
+                            />
+                        </InputWrapper>
+                        {errorMessage.password}
+                        <div>
+                            <input
+                                type="submit"
+                                value="Login"
+                                style={{
+                                    backgroundColor: "green",
+                                    borderRadius: "5px",
+                                    width: "200px",
+                                    height: "30px",
+                                    margin: "10px",
+                                }}
+                            />
+                        </div>
+                        {errorMessage.apiError}
+                    </form>
+                </div>
+            )}
+        </>
+    );
 }
-
 
 export default Login;
---
+++ src/components/styled/Title.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    color: var(--blue);
+    text-align: center;
+`
+
+export default Title
