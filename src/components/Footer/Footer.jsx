65341538db9031398ea127a075dd4008c728d88c
Nassy Kova <mailtosummer@gmail.com>
2023-03-01 13:07:13 +1100

    Change Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Admin, Wrapper } from "../Footer/Footer.styles";
 import Socials from "../Socials/Socials";
-import { Link } from "@chakra-ui/react";
+// import { Link } from "@chakra-ui/react";
 import DividerLine from "../Divider/Divider";
-
+import { Link } from "react-router-dom";
+import Login from "../Login";
 const Footer = () => {
     return (
         <Wrapper>
             <DividerLine />
 
-                <Socials />
+            <Socials />
 
             <Admin>
-                <Link color="#43d9f0" href="https://chakra-ui.com" l>
+                <Link to="/login" relative="login" element={<Login/>} color="#43d9f0">
                     admin login
                 </Link>
+
             </Admin>
         </Wrapper>
     );
