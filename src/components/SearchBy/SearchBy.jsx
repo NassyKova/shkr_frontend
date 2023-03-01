ac036196a7a875fd615666af2fcd9167edb0b20c
Nassy Kova <mailtosummer@gmail.com>
2023-03-01 15:55:24 +1100

    Add Search By

---
+++ src/components/SearchBy/SearchBy.jsx
@@ -0,0 +1,22 @@
+const SearchByFizzy = () => {
+    return <div>SearchByFizzy</div>;
+};
+
+const SearchByFruity = () => {
+    return <div>Fruity</div>;
+};
+
+const SearchByHeavy = () => {
+    return <div>Heavy</div>;
+};
+
+const SearchByNoAlc = () => {
+    return <div>SearchByNoAlc</div>;
+};
+
+const SearchByName = () => {
+    return <div>SearchByName</div>;
+};
+
+
+export {SearchByFizzy, SearchByFruity, SearchByHeavy, SearchByName, SearchByNoAlc}
