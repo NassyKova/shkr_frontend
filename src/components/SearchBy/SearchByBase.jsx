e360e261b6beb0567d4064d93cc774746acafaea
Nassy Kova <mailtosummer@gmail.com>
2023-03-03 09:53:31 +1100

    Add Search Bar

--- src/components/SearchBy/SearchByBase.jsx
+++ src/components/SearchBy/SearchByBase.jsx
@@ -1,18 +1,23 @@
-import { Button, Wrapper } from "./SearchByBase.styles"
+import { Button, Wrapper } from "./SearchByBase.styles";
+import { Link } from "react-router-dom";
 
 const SearchByBase = () => {
-    return(
+    return (
         <Wrapper>
-        <Button>Rum</Button>
-        <Button>Vodka</Button>
-        <Button>Bourbon</Button>
-        <Button>Gin</Button>
-        <Button>Tequila</Button>
-        <Button>Brandy</Button>
-        <Button>Scotch</Button>
-        <Button>Triple Sec</Button>
-        </Wrapper>
-    )
-}
+            <Button>Rum</Button>
+            <Link to="/finder/base/vodka" style={{ textDecoration: "none" }}>
+                {" "}
+                <Button>Vodka</Button>
+            </Link>
 
-export default SearchByBase
+            <Button>Bourbon</Button>
+            <Button>Gin</Button>
+            <Button>Tequila</Button>
+            <Button>Brandy</Button>
+            <Button>Scotch</Button>
+            <Button>Triple Sec</Button>
+        </Wrapper>
+    );
+};
+
+export default SearchByBase;
