fbed7ead25ef437fdeb6a8a8d6bb4b746f635cee
Nassy Kova <mailtosummer@gmail.com>
2023-03-08 11:59:41 +1100

    SearchByName WIP

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,37 @@
-import { Wrapper, Img,Title, Ingridients,  Grid} from "./Product.Styles";
+import { Wrapper, Img, Title, Ingridients, Grid } from "./Product.Styles";
 import React from "react";
 
-
 function Product(props) {
-    const item = props.productInfo
+    const item = props.productInfo;
     return (
         <Grid>
-        <Wrapper style={{cursor:'pointer'}} onClick={() => {
-            props.setItem(item)
-        }}>
-            <Img>
-                <img src={item.strDrinkThumb} style={{ width: 300, height: 300 }} alt="drink"></img>
-            </Img>
-            <Title>{item.strDrink}</Title>
-            <Ingridients key={"ingrindient-item-" + item.id}>{item.strIngredient1}{", "}{item.strIngredient2}{", "}{item.strIngredient3}{", "}{item.strIngredient4}{", "}{item.strIngredient5}</Ingridients>
-            <div>{item.strInstructions}</div>
-        </Wrapper>
+            <Wrapper
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                    props.setItem(item);
+                }}
+            >
+                <Img>
+                    <img
+                        src={item.strDrinkThumb}
+                        style={{ width: 300, height: 300 }}
+                        alt="drink"
+                    ></img>
+                </Img>
+                <Title>{item.strDrink}</Title>
+                <Ingridients key={"ingrindient-item-" + item.id}>
+                    {item.strIngredient1}
+                    {", "}
+                    {item.strIngredient2}
+                    {", "}
+                    {item.strIngredient3}
+                    {", "}
+                    {item.strIngredient4}
+                    {", "}
+                    {item.strIngredient5}
+                </Ingridients>
+                <div>{item.strInstructions}</div>
+            </Wrapper>
         </Grid>
     );
 }
--- src/components/SearchBy/SearchByName.jsx
+++ src/components/SearchBy/SearchByName.jsx
@@ -1,81 +1,35 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./List";
+import "./../../App.css";
 
-const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(1),
-        width: "auto",
-    },
-}));
+function SearchBar() {
+    const [inputText, setInputText] = useState("");
+    let inputHandler = (e) => {
+        //convert input text to lower case
+        var lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+    };
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("sm")]: {
-            width: "12ch",
-            "&:focus": {
-                width: "20ch",
-            },
-        },
-    },
-}));
-
-export default function SearchAppBar() {
     return (
-        <Box sx={{ flexGrow: 1 }} style={{
-            display: "flex",
-            flexWrap: "wrap",
-            alignItems: "center",
-            justifyContent: "center",
-            margin: "20px",
-            
-            }}>
-
-                    <IconButton 
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                    >
-                    </IconButton>
-                    <Search style={{color: "#00a18e"}}>
-                        <SearchIconWrapper >
-                            <SearchIcon  />
-                        </SearchIconWrapper>
-                        <StyledInputBase  
-                            placeholder="Search…"
-                            inputProps={{ "aria-label": "search" }}
-                        />
-                    </Search>
-
-        </Box>
+        <div className="main">
+            <div className="search">
+                <TextField
+                    style={{
+                        backgroundColor: "white",
+                        color: "#00a18e",
+                        borderRadius: "15px",
+                    }}
+                    id="outlined-basic"
+                    onChange={inputHandler}
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                />
+            </div>
+            <List input={inputText} />
+        </div>
     );
 }
+
+export default SearchBar;
