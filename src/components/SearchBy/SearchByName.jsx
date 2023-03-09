23173e4b1510714c27172a49fa50aa71e0221050
Nassy Kova <mailtosummer@gmail.com>
2023-03-09 12:22:02 +1100

    SearchByName WIP

--- src/components/SearchBy/SearchByName.jsx
+++ src/components/SearchBy/SearchByName.jsx
@@ -1,19 +1,46 @@
 import { React, useState } from "react";
 import TextField from "@mui/material/TextField";
 import List from "./List";
-import "./../../App.css";
+import axios from "axios";
 
 function SearchBar() {
-    const [inputText, setInputText] = useState("");
+    const [drinkName, setDrinkName] = useState("");
     let inputHandler = (e) => {
         //convert input text to lower case
         var lowerCase = e.target.value.toLowerCase();
-        setInputText(lowerCase);
+        setDrinkName(lowerCase);
     };
 
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    function onClickSearchByName() {
+        axios
+            .get(`/drinks/name/${drinkName}`)
+            .then((res) => res.data)
+            .then((json) => {
+                const newItems = json.drinks.map((product) => {
+                    return product;
+                });
+                console.log(newItems);
+
+                setData(newItems.slice(0, 5));
+                setIsLoading(false);
+            });
+    }
+
     return (
-        <div className="main">
-            <div className="search">
+        <div
+            style={{
+                display: "flex",
+                height: "100vh",
+                width: "100%",
+                alignItems: "center",
+                flexDirection: "column",
+                rowGap: "20px",
+            }}
+        >
+            <div style={{ width: "30%" }}>
                 <TextField
                     style={{
                         backgroundColor: "white",
@@ -26,8 +53,21 @@ function SearchBar() {
                     fullWidth
                     label="Search"
                 />
+                <button
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        backgroundColor: "green",
+                        borderRadius: "15px",
+                        margin: "10px",
+                    }}
+                    onClick={onClickSearchByName()}
+                >
+                    Search
+                </button>
             </div>
-            <List input={inputText} />
+            {/* <List input={drinkName} /> */}
         </div>
     );
 }
