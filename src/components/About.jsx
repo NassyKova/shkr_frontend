e845a3df8a3300c7b3c22f5f825fc0e085e47af3
Nassy Kova <mailtosummer@gmail.com>
2023-03-11 10:05:06 +1100

    About us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,34 @@
+import Title from "./styled/Title";
 const About = () => {
     return (
-        <div style={{ 
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: "2rem" }}>
-            <>About Us</>
+        <div>
+            <Title>About Us</Title>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontSize: "2rem",
+                    color: "#00a18e ",
+                    margin: "5rem 20rem",
+                }}
+            >
+                {" "}
+                The purpose of this website is to create an easy-to-use cocktail
+                recipe book for liquor distributors to provide to bartenders as
+                a means of promoting their product. Arming bartenders with easy
+                access to a multitude of recipes will allow them to keep the
+                distributor's products front of mind and more importantly front
+                of house and in the customer's hands. The website includes a
+                library of drink recipes, searchable by ingredient, name, or
+                various other genres according to taste. Upon searching for an
+                ingredient, a random drink using that ingredient will be
+                provided with recipe, picture and method. The app will also
+                provide admin users the ability to remove certain drinks from
+                the randomizing function so that specific drinks can be excluded
+                from the search results, as well as manipulating the database to
+                maintain brands across different recipes.
+            </div>
         </div>
     );
 };
--- src/components/admin/AdminOptions.styles.jsx
+++ src/components/admin/AdminOptions.styles.jsx
@@ -3,35 +3,26 @@ import styled from "styled-components";
 export const MainWrapper = styled.div`
     display: flex;
     align-items: center;
-
-
     flex-wrap: wrap;
     justify-content: center;
     padding: 20px;
     border-radius: 10px;
     transition: 0.3s;
     box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.75);
-
-    
-`
+`;
 
 export const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
-    margin:0px 0;
-
-
-    
+    margin: 0px 0;
 `;
 
 export const RebrandWrapper = styled.div`
-        display: flex;
+    display: flex;
     flex-wrap: wrap;
     align-items: left;
     justify-content: left;
-    margin: 50px;
-
-;
-`
+    margin: 50px; ;
+`;
