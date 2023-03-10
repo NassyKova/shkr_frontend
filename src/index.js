51acaf8561fca454f9c00b9c987db9e96fc7867a
Nassy Kova <mailtosummer@gmail.com>
2023-03-10 19:27:36 +1100

    Admin WIP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
 // axios.defaults.baseURL="https://www.thecocktaildb.com/api/json/v2/9973533"
