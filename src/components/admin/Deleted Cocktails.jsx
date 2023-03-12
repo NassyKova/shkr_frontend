2c57aaeea61bd3b060d568d214ca5c687f9ece8b
Nassy Kova <mailtosummer@gmail.com>
2023-03-12 21:34:45 +1100

    Working

--- src/components/admin/Deleted Cocktails.jsx
+++ src/components/admin/Deleted Cocktails.jsx
@@ -1,6 +1,5 @@
 import Title from "../styled/Title";
 import { React, useEffect, useState } from "react";
-import Button from "@mui/material/Button";
 import axios from "axios";
 import SpinnerBlue from "../Spinner";
 import Box from "@mui/material/Box";
@@ -19,6 +18,7 @@ const InputWrapper = styled.div`
 // Define a functional component called ForbiddenCocktails
 const ForbiddenCocktails = () => {
     const [isLoading, setIsLoading] = useState(true);
+    const [setReloadPage] = useState(false);
 
     // Define state variables using the useState hook
     const [forbiddenCocktailsData, setForbiddenCocktailsData] = useState([]);
@@ -39,9 +39,10 @@ const ForbiddenCocktails = () => {
                 // console.log(typeof filteredResponse)
                 // Update the state variable with the fetched data
                 setForbiddenCocktailsData(res.data);
+
                 console.log("set forbidden cocktails data to ");
                 console.log(forbiddenCocktailsData);
-
+                setReloadPage(false);
                 // Update the isLoading state variable to false
                 setIsLoading(false);
             })
@@ -49,26 +50,19 @@ const ForbiddenCocktails = () => {
                 // Log any errors to the console
                 console.log(error.message);
             });
-            // eslint-disable-next-line
-    }, []);
+        // eslint-disable-next-line
+    }, [setReloadPage]);
 
-    function DeleteFromForbiddenCocktails(drink) {
-        console.log(
-            "accesing state from DeleteFromForbiddenCocktails: " +
-                forbiddenCocktailsData
-        );
-        if (!drink) {
-            console.log("Search input is empty");
+    function DeleteFromForbiddenCocktails() {
+        const drink = searchInput
+        if (!forbiddenCocktailsData.includes(drink)) {
             return;
         }
-        console.log(`deleting ${drink} from forbidden cocktails`);
         axios
             .patch("/products/forbidden/remove", { drink: drink })
             .then((res) => {
-                console.log("Cocktail is back in the database");
-                console.log(res);
-                console.log("Before setForbiddenCocktailsData([])");
-                setForbiddenCocktailsData([]);
+                setForbiddenCocktailsData(res.data.forbidden);
+                setReloadPage(true);
                 // forbiddenCocktailsData.indexOf(name??)
                 console.log("after setForbiddenCocktailsData");
             })
@@ -83,40 +77,42 @@ const ForbiddenCocktails = () => {
     const HandleSearchToDelete = (e) => {
         e.preventDefault();
         setSearchInput(e.target.value);
+        console.log(searchInput)
     };
 
     return (
         // Return JSX with the components and data
         <div>
-            <input
-                type="search"
-                placeholder="Copy cocktail name here"
-                onChange={HandleSearchToDelete}
-                value={searchInput}
-                style={{
-                    color: "black",
-                    borderRadius: "15px",
-                    height: "50px",
-                    width: "300px",
-                    cursor: "pointer",
-                    padding: "20px",
-                }}
-            />
-
-            <Button
-                className="menu"
-                onSubmit={DeleteFromForbiddenCocktails(searchInput)}
-                style={{
-                    color: "black",
-                    textTransform: "none",
-                    backgroundColor: "#d585b2",
-                    borderRadius: "10px",
-                    marginBottom: "20px",
-                }}
-                variant="outlined"
-            >
-                Remove from the removed cocktails
-            </Button>
+            <form onSubmit={DeleteFromForbiddenCocktails()}>
+                <label>
+                    Copy cocktail name here:
+                    <input
+                    onChange={HandleSearchToDelete}
+                        style={{
+                            color: "black",
+                            borderRadius: "15px",
+                            height: "50px",
+                            width: "300px",
+                            cursor: "pointer",
+                            padding: "20px",
+                        }}
+                        type="text"
+                        value={searchInput}
+                    />
+                </label>
+                <input
+                    style={{
+                        color: "black",
+                        textTransform: "none",
+                        backgroundColor: "#d585b2",
+                        borderRadius: "10px",
+                        marginBottom: "20px",
+                        cursor: "pointer",
+                    }}
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
 
             <div
                 style={{
@@ -132,12 +128,13 @@ const ForbiddenCocktails = () => {
 
             {forbiddenCocktailsData.map((item, key) => {
                 return (
-                    <InputWrapper>
+                    <InputWrapper key={"forbidden-" + item}>
                         {/* // Check if isLoading is true, if so render a spinner component */}
                         {isLoading ? (
                             // Add a Box component for better styling
                             <Box
                                 sx={{
+                                    
                                     display: "flex",
                                     justifyContent: "center",
                                     alignItems: "center",
