d57cd9c9a9b96866a349ece72d3e99794eb1d877
Nassy Kova <mailtosummer@gmail.com>
2023-03-24 09:50:24 +1100

    comment out admin login in footer"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { Admin, Wrapper } from "../Footer/Footer.styles";
+import { Wrapper } from "../Footer/Footer.styles";
+// import { Admin, Wrapper } from "../Footer/Footer.styles";
 import Socials from "../Socials/Socials";
 import DividerLine from "../Divider/Divider";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <Wrapper>
             <DividerLine />
             <Socials />
-            <Admin>
+            {/* <Admin>
                 <Link to="/admin" relative="login" color="#43d9f0">
                     admin
                 </Link>
-            </Admin>
+            </Admin> */}
         </Wrapper>
     );
 };
